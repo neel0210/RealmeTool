7f82f6032890ad756a2d89cc33153560489a806e
Neel <neeljadhav0210@gmail.com>
2022-03-19 05:57:19 +0530

    Update sec.sh

--- tools/debloat/sec.sh
+++ tools/debloat/sec.sh
@@ -54,7 +54,29 @@ adb shell pm uninstall -k --user 0 com.google.android.keep
 adb shell pm uninstall -k --user 0 com.google.android.contacts 
 adb shell pm uninstall -k --user 0 com.google.android.apps.messaging
 adb shell pm uninstall -k --user 0 com.google.android.dialer
+adb shell pm uninstall -k --user 0 com.heytap.market
+adb shell pm uninstall -k --user 0 com.opos.cs
+adb shell pm uninstall -k --user 0 com.heytap.browser
+adb shell pm uninstall -k --user 0 com.heytap.music
+adb shell pm uninstall -k --user 0 com.coloros.video
+adb shell pm uninstall -k --user 0 com.oppo.sos
+adb shell pm uninstall -k --user 0 com.heytap.usercenter
+adb shell pm uninstall -k --user 0 com.heytap.cloud
+adb shell pm uninstall -k --user 0 com.heytap.themestore 
+adb shell pm uninstall -k --user 0 com.finshell.fin
+adb shell pm uninstall -k --user 0 com.coloros.securepay
+adb shell pm uninstall -k --user 0 com.coloros.backuprestore
+adb shell pm uninstall -k --user 0 com.coloros.assistantscreen
+adb shell pm uninstall -k --user 0 com.redteamobile.roaming
+adb shell pm uninstall -k --user 0 com.coloros.compass2
+adb shell pm uninstall -k --user 0 com.oppo.quicksearchbox
+adb shell pm uninstall -k --user 0 com.realme.link
+adb shell pm uninstall -k --user 0 com.coloros.onekeylockscreen
+adb shell pm uninstall -k --user 0 com.coloros.videoeditor
+adb shell pm uninstall -k --user 0 com.realmecomm.app
+adb shell pm uninstall -k --user 0 com.heytap.quickgame
+adb shell pm uninstall -k --user 0 com.realmestore.app
+adb shell pm uninstall -k --user 0 com.heytap.pictorial
 #adb shell pm uninstall -k --user 0 
 #adb shell pm uninstall -k --user 0 
-echo done
-
+#echo done
